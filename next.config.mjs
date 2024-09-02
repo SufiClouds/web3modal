d48e582a742c0a3663800a70baa539c50101a8d5
Muhammad Sufyan <sufiwork14@gmail.com>
2024-09-02 17:44:28 +0500

    hello

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+// Path: next.config.js
+const nextConfig = {
+    webpack: config => {
+      config.externals.push('pino-pretty', 'lokijs', 'encoding')
+      return config
+    }
+  }
